3c384e637e2ae0609425cd723d31dabf8fc9b513
MrDoritos <ianmoore322@gmail.com>
2019-04-05 18:14:00 +0200

    Added radians / degrees conversions

--- headers/imath.h
+++ headers/imath.h
@@ -1,15 +1,8 @@
-//#include <cmath>
+#pragma once
 
 double dist(double x0, double y0, double x1, double y1);
 float distf(float x0, float y0, float x1, float y1);
-
-/*
-inline double dist(double x0, double y0, double x1, double y1) {
-	return sqrt(((x1 - x0) * (x1 - x0)) + ((y1 - y0) * (y1 - y0)));
-}
-
-//Reimpl in float only
-inline float distf(float x0, float y0, float x1, float y1) {
-	return dist(x0, y0, x1, y1);
-}
-*/
+double rad(double deg);
+double deg(double rad);
+float radf(float deg);
+float degf(float rad);
--- source/imath.cpp
+++ source/imath.cpp
@@ -1,6 +1,11 @@
 #include <cmath>
 
+#define PI 3.1415926
+#define D_PI 6.2831853
+
 #define DIST__ (sqrt(((x1 - x0) * (x1 - x0)) + ((y1 - y0) * (y1 - y0))))
+#define RAD__(xs) (xs * PI / 180)
+#define DEG__(xs) (xs * 180 / PI)
 
 double dist(double x0, double y0, double x1, double y1) {
 	return DIST__;
@@ -10,4 +15,22 @@ float distf(float x0, float y0, float x1, float y1) {
 	return DIST__;
 }
 
-#undef DIST
+double rad(double deg) {
+	return RAD__(deg);
+}
+
+float radf(float deg) {
+	return RAD__(deg);
+}
+
+double deg(double rad) {
+	return DEG__(rad);
+}
+
+float degf(float rad) {
+	return DEG__(rad);
+}
+
+#undef DIST__
+#undef RAD__
+#undef DEG__
